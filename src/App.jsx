515da92a25fd44f8de8a456de5c22d97b1d1c736
Nurgul Madanbekova <nurgulya792@gmail.com>
2024-12-09 12:39:55 +0600

    create project hb

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Happy Birthday to You,James!</h1>
+
+      <p>
+        Пусть мечты сбудутся!
+      </p>
+
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/5u4xTa3LR2U?si=Rif5B_WHZMt9NOGa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+  );
+}
